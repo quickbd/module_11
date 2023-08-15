4e8ba8697a9a2539a3252ee6d7cd627858e6a00a
Md.Hasanat Zamil <zamil_surfserver@yahoo.com>
2023-08-15 19:27:06 +0600

    update blog link

--- app/blog/page.js
+++ app/blog/page.js
@@ -13,23 +13,22 @@ export default function Blog() {
       <div className="grid  grid-cols-3 grid-rows-4 gap-2">
         {blogData.map((blog) => (
           <div class="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700  ">
-            <a href={`blog/${blog.slug}`}>
+            <Link href={`blog/${blog.slug}`}>
               <img
                 class="rounded-t-lg"
                 src={`/images/post/${blog.image}`}
                 alt=""
               />
-            </a>
+            </Link>
             <div class="p-5">
-              <a href={`blog/${blog.slug}`}>
+              <Link href={`blog/${blog.slug}`}>
                 <h5 class="mb-2  {styles.red} font-bold tracking-tight text-gray-900 dark:text-white">
                   {blog.title ?? ""}{" "}
-                 
                 </h5>
-              </a>
- <div className="float-end text-end text-xs font-light">
-                    Date: {blog.date}
-                  </div>
+              </Link>
+              <div className="float-end text-end text-xs font-light">
+                Date: {blog.date}
+              </div>
               <Link
                 href={`blog/${blog.slug}`}
                 class="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
